824cb2af79399253a1ee26ef345d1bfc15540685
Ny&u15LXg5KO7tQj <yeminfu4319@gmail.com>
2026-03-01 04:47:48 +1000

    up

--- app/form.tsx
+++ app/form.tsx
@@ -41,7 +41,8 @@ export default function HookForm() {
     try {
       // Имитация API запроса
       // await new Promise(resolve => setTimeout(resolve, 1000));
-      console.log('Submitted:', data);
+      console.log('Submitted:', JSON.stringify(data, null, 2));
+      alert('ok');
       // alert('✅ Форма отправлена!');
       // reset();
     } catch (error) {
@@ -55,50 +56,84 @@ export default function HookForm() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="max-w-lg mx-auto p-6 bg-white rounded-lg shadow-md space-y-5">
-      <h2 className="text-2xl font-bold text-gray-800 text-center">
+      <h2 className="text-2xl font-bold text-gray-800 text-center m-0">
         Запись
         {/* <div>usernameValue: {dayjs(date).format('DD.MM.YYYY')}</div> */}
 
       </h2>
 
-      <table className='min-w-low bg-white border border-gray-200'>
+      <table className='min-w-full bg-white border-collapse border border-slate-1000'>
         <tbody>
           <tr className="hover:bg-gray-50 transition">
             <th className='px-1 py-1  text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>state</th>
             <td className='px-1 py-1  whitespace-nowrap text-sm text-gray-900"'>{state}</td>
           </tr>
 
-          <tr className="hover:bg-gray-50 transition">
+          {service && <tr className="hover:bg-gray-50 transition">
             <td className='px-1 py-1  text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Услуга:</td>
             <td className='px-1 py-1  whitespace-nowrap text-sm text-gray-900"'>{service}</td>
-          </tr>
+          </tr>}
 
           {/* {service && } */}
 
-          <tr className="hover:bg-gray-50 transition">
+          {date && <tr className="hover:bg-gray-50 transition">
             <td className='px-1 py-1  text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Дата:</td>
             <td className='px-1 py-1  whitespace-nowrap text-sm text-gray-900"'>{dayjs(date).isValid() && dayjs(date).format('DD.MM.YYYY')}</td>
-          </tr>
+          </tr>}
 
           {/* {service && } */}
 
-          <tr className="hover:bg-gray-50 transition">
+          {time && <tr className="hover:bg-gray-50 transition">
             <td className='px-1 py-1  text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Время:</td>
             <td className='px-1 py-1  whitespace-nowrap text-sm text-gray-900"'>{dayjs(time).isValid() && <>{dayjs(time).format('HH:mm')}</>}</td>
-          </tr>
+          </tr>}
 
           {/* {date && } */}
         </tbody>
       </table>
 
       {(() => {
+        if (state === 'addDate') {
+          return <>
+            <div className="">
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="w-full bg-blue-600 text-white py-1 px-1  rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition font-medium disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center"
+              >
+                {isSubmitting ? (
+                  <>
+                    <svg className="animate-spin -ml-1 mr-2 h-4 w-4 text-white" fill="none" viewBox="0 0 24 24">
+                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
+                      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z" />
+                    </svg>
+                    Отправка...
+                  </>
+                ) : (
+                  'Сохранить заявку'
+                )}
+              </button>
+              <div style={{ height: 5 }}></div>
+              <button
+                type="button"
+                disabled={isSubmitting}
+                onClick={() => {
+                  window.location.reload();
+                }}
+                className="w-full outline-solid outline-red-300 text-red-500 py-1 px-1  rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition font-medium disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center"
+              >
+                Заново
+              </button>
+            </div>
+          </>
+        }
         if (state === 'addedServiceType') {
           return <>
             Выберите подходящую дату и время
             <HookFormCalendar getDateTime={(value: any) => {
               setValue('date', value);
               setValue('time', value);
-              setState('addDate')
+              setState('addDate');
 
             }} />
           </>
@@ -132,9 +167,10 @@ export default function HookForm() {
                   type="button"
                   // disabled={isSubmitting}
                   onClick={() => {
+                    if (!service) return;
                     setState('addedServiceType')
                   }}
-                  className="w-full bg-blue-600 text-white py-3 px-4 rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition font-medium disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center"
+                  className="w-full bg-blue-600 text-white py-1 px-1  rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition font-medium disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center"
                 >
                   {isSubmitting ? (
                     <>
@@ -157,10 +193,11 @@ export default function HookForm() {
 
 
       {/* Кнопка отправки */}
+
       {/* <button
         type="submit"
         disabled={isSubmitting}
-        className="w-full bg-blue-600 text-white py-3 px-4 rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition font-medium disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center"
+        className="w-full bg-blue-600 text-white py-1 px-1  rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition font-medium disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center"
       >
         {isSubmitting ? (
           <>
